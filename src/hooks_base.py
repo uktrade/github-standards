5e29ec8cd12c512a94c4cf92bce6a9ca3826bc4a
Chris Hopkins <102232401+chopkinsmade@users.noreply.github.com>
2025-10-02 13:49:16 +0100

    Add an env var to the docker image to force hook checks (#23)

--- src/hooks_base.py
+++ src/hooks_base.py
@@ -6,7 +6,7 @@ from pathlib import Path
 from typing import List
 
 
-from src.config import PRE_COMMIT_FILE
+from src.config import FORCE_HOOK_CHECKS, PRE_COMMIT_FILE
 
 
 logger = logging.getLogger()
@@ -22,12 +22,14 @@ class Hook(ABC):
         raise NotImplementedError()
 
     def _skip_check(self) -> bool:
-        """If this check is ran in the dbt-hooks repository, it will always fail as we use a local hook implementation for running the pre-commit hooks. This ensures we are always running the latest local version, and will catch any development errors early before creating a tagged docker image as part of releasing.
+        """If this check is ran in the dbt-hooks repository, it will always fail as we use a local hook implementation
+        for running the pre-commit hooks. This ensures we are always running the latest local version, and will catch
+        any development errors early before creating a tagged docker image as part of releasing.
 
         Returns:
             bool: Whether this check should be skipped
         """
-        return "site-packages" not in __file__
+        return FORCE_HOOK_CHECKS == "0"
 
     def validate_hook_settings(self) -> bool:
         if self._skip_check():
@@ -35,22 +37,22 @@ class Hook(ABC):
             return True
 
         if not Path(PRE_COMMIT_FILE).exists():
-            logger.info("File %s does not exist in this repository. This file must be present", PRE_COMMIT_FILE)
+            logger.debug("File %s does not exist in this repository. This file must be present", PRE_COMMIT_FILE)
             return False
 
         with open(PRE_COMMIT_FILE, "r") as file:
             config = yaml.safe_load(file)
 
             if "repos" not in config:
-                logger.info("File %s does not contain a repo tag", PRE_COMMIT_FILE)
+                logger.debug("File %s does not contain a repo tag", PRE_COMMIT_FILE)
                 return False
 
             dbt_hook_repo = list(filter(lambda x: "https://github.com/uktrade/dbt-hooks" in x["repo"], config["repos"]))
             if not dbt_hook_repo:
-                logger.info("File %s does not contain the dbt hooks repo", PRE_COMMIT_FILE)
+                logger.debug("File %s does not contain the dbt hooks repo", PRE_COMMIT_FILE)
                 return False
             if len(dbt_hook_repo) != 1:
-                logger.info("File %s can only contain one dbt-hooks repo entry", PRE_COMMIT_FILE)
+                logger.debug("File %s can only contain one dbt-hooks repo entry", PRE_COMMIT_FILE)
                 return False
 
             return self._validate_hook_settings(dbt_hook_repo[0])
